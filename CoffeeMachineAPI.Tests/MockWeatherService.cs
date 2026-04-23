7c7b7b602a4a6c20a025ee4cf7d69ff458b8df1e
empul <126280204+empul@users.noreply.github.com>
2026-04-24 07:17:52 +0800

    Add files via upload

---
+++ CoffeeMachineAPI.Tests/MockWeatherService.cs
@@ -0,0 +1,33 @@
+﻿using CoffeeMachineAPI.Interfaces;
+using Moq;
+
+namespace CoffeeMachineAPI.Tests
+{
+    // Simple mock for testing
+    public class MockWeatherService : IWeatherService
+    {
+        private readonly double? _temperature;
+
+        public MockWeatherService(double? temperature)
+        {
+            _temperature = temperature;
+        }
+
+        public Task<double?> GetCurrentTemperatureAsync()
+        {
+            return Task.FromResult(_temperature);
+        }
+    }
+
+    // Helper to create mock with Moq
+    public static class WeatherServiceMockHelper
+    {
+        public static Mock<IWeatherService> CreateMock(double? temperature)
+        {
+            var mock = new Mock<IWeatherService>();
+            mock.Setup(x => x.GetCurrentTemperatureAsync())
+                .ReturnsAsync(temperature);
+            return mock;
+        }
+    }
+}
