329680e16409a1fb09ddf7fd723f4c3f0d61138e
empul <126280204+empul@users.noreply.github.com>
2026-04-24 07:20:52 +0800

    Add files via upload

--- Controllers/CoffeeController.cs
+++ Controllers/CoffeeController.cs
@@ -16,7 +16,7 @@ namespace CoffeeMachineAPI.Controllers
         }
 
         [HttpGet("brew-coffee")]
-        public IActionResult BrewCoffee()
+        public async Task<IActionResult> BrewCoffee()
         {
             // Requirement 3: April 1st returns 418 I'm a teapot
             if (_coffeeService.IsAprilOne())
@@ -33,10 +33,12 @@ namespace CoffeeMachineAPI.Controllers
                 return StatusCode(503);
             }
 
-            // Requirement 1: Return 200 OK with JSON response
+            // Requirement 1: Return 200 OK with JSON response (refactored Weather Service)
+            var message = await _coffeeService.GetBrewMessageAsync();
+
             var response = new CoffeeResponse
             {
-                Message = "Your piping hot coffee is ready",
+                Message = message,  
                 Prepared = _coffeeService.GetCurrentTimestamp()
             };
 
